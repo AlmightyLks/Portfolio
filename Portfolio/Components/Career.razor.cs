4928d8f53a32b0cbdb33d5a592b0e2a0075e5887
AlmightyLks <almightylks@protonmail.com>
2023-06-04 16:59:24 +0200

    Mobile adjustments

---
+++ Portfolio/Components/Career.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using MudBlazor.Services;
+
+namespace Portfolio.Components;
+
+public partial class Career
+{
+    [Inject]
+    public IBreakpointService BreakpointListener { get; set; } = null!;
+
+    public bool IsMobile { get; set; }
+    public TimelinePosition TimelinePosition
+        => IsMobile ? TimelinePosition.Left : TimelinePosition.Alternate;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await BreakpointListener.SubscribeAsync((breakpoint) =>
+        {
+            IsMobile = breakpoint == Breakpoint.Xs;
+            StateHasChanged();
+        });
+    }
+}
